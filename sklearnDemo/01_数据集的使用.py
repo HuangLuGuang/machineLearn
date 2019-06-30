1c5272384c6f61ce8d3ec6ee746c634407610eda
huanglg <835923123@qq.com>
2019-06-30 18:52:50 +0800

    Initial commit

---
+++ sklearnDemo/01_数据集的使用.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @createTime    : 2019/6/26 9:23
+# @author  : Huanglg
+# @fileName: 01_数据集的使用.py
+# @email: luguang.huang@mabotech.com
+from sklearn.datasets import load_iris, load_boston
+
+iris = load_boston()
+print(iris)
---
+++ sklearnDemo/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @createTime    : 2019/6/26 9:22
+# @author  : Huanglg
+# @fileName: __init__.py.py
+# @email: luguang.huang@mabotech.com
