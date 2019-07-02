0ed757bb8395062a03c946a03f6d9851d5e006da
huanglg <835923123@qq.com>
2019-07-02 23:15:31 +0800

    i

--- sklearnDemo/04_文本特征提取.py
+++ sklearnDemo/04_文本特征提取.py
@@ -4,7 +4,8 @@
 # @fileName: 04_文本特征提取.py
 # @email: luguang.huang@mabotech.com
 import jieba
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
 
 def text_count_demo():
     """
@@ -50,6 +51,27 @@ def text_chinese_count_demo2():
     print(transfer.get_feature_names())
 
 
+def text_chinese_tfidf_demo():
+    """
+    对中文提取
+    :return:
+    """
+    data = ["一种还是一种今天很残酷，明天更残酷，后天很美好，但绝对大部分是死在明天晚上，所以每个人不要放弃今天。",
+            "我们看到的从很远星系来的光是在几百万年之前发出的，这样当我们看到宇宙时，我们是在看它的过去。",
+            "如果只用一种方式了解某样事物，你就不会真正了解它。了解事物真正含义的秘密取决于如何将其与我们所了解的事物相联系。"]
+    # 将原始数据转换成分好词的形式
+    text_list = []
+    for send in data:
+        send = cut_world(send)
+        text_list.append(send)
+
+    # 实例化转换类
+    transfer = TfidfVectorizer(stop_words=["一种", "不会", "不要"])
+    data = transfer.fit_transform(text_list)
+    print(data.toarray())
+    print(transfer.get_feature_names())
+
+
 if __name__ == '__main__':
     # text_count_demo()
-    text_chinese_count_demo2()
+    text_chinese_tfidf_demo()
